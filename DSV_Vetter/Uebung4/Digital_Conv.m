8267e20b3cf4641442132462d11cf4614758f9ad
Marcel Baertschi <marcel.baertschi@gmx.ch>
2013-04-22 09:44:22 +0200

    worked on DSV

--- DSV_Vetter/Uebung4/Digital_Conv.m
+++ DSV_Vetter/Uebung4/Digital_Conv.m
@@ -2,11 +2,14 @@
 
 close all;
 clear all;
-
+t = 0:0.01:2*pi;
 % x = [1 0 1 0 1 0 1 0 1]';
-x = [0 1 2 3 4 5 6 0 0 0 0 0]';
+% x = [0 1 2 3 4 5 6 0 0 0 0 0]';
+x = sin(t)';
+% g = [1 -1]';
+% g = [1 1]';
+g = [1 0 0 0 0 1]';
 
-g = [1 -1]';
 
 N = length(x);
 p = length(g)-1;
@@ -18,7 +21,16 @@ for n = p+1:N;
     y(n) = g'*phi_n;
 end
 
+% Kontrolle mit Matlabfunktion
+y_k = conv(x, g, 'same');
+
+
+
+disp('Eigene Fkt.: '); y
+disp('Matlab Fkt.: '); y_k'
 plot(x);
 hold all;
 stairs(y);
-legend('Eingangssignal', 'Ableitung durch Falten mit [1 -1]');
+
+legende = int2str(g');
+legend('Eingangssignal', 'Ableitung durch Falten mit ['legende ']');
