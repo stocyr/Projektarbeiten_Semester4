aac0239a49a1f00a2adda5cf99e9e209e72ae83c
Cyril Stoller <cyril.stoller@gmail.com>
2013-04-26 11:58:43 +0200

    ztransformation klappe die erste

--- DSV_Vetter/Uebung5/ztransformation.m
+++ DSV_Vetter/Uebung5/ztransformation.m
@@ -1,10 +1,39 @@
-g = [1 1 1 1 1];
-Nf = 256;
+% close all;
+% clear all;
+
+% Gleitender Mittelwert
+h = [1 0 0 0 0 0 0 0 0 1]';
+
+% Kaiserförmige Impulsantwort
+%h = kaiser(100, 10);
+%h = h(end/2:end);
+
+% Eigene freqz implementation
+Nf = 1024;
 f = [0:1/Nf:1/2];
 z = exp(j*2*pi*f);
-Gf = zeros(size(f));
-for n = 1:length(g)
-    Gf = Gf + g(n)*z.^(n-1);
+H = zeros(size(f));
+for n = 1:length(h)
+    H = H + h(n)*z.^-(n-1);
 end
+% Matlab interne freqz funktion
+%H = freqz(h);
 
-plot(Gf);
+subplot(2,2,[1 3]);
+stem(h, 'linewidth', 2);
+set(gca, 'ylim', [-1.1 1.1], 'xlim', [0.9 10.1]);
+title('Impulsantwort im Zeitbereich');
+grid on
+
+subplot(2,2,2);
+loglog(abs(H)*20, 'linewidth', 2);
+xlabel('Frequenz [Hz]');
+ylabel('Gain [dB]');
+title('Frequenzgang des Filters');
+grid on
+
+subplot(2,2,4);
+semilogx(angle(H)*180/pi, 'linewidth', 2);
+ylabel('Phase [°]');
+xlabel('Frequenz [Hz]');
+grid on
