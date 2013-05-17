571a960da3fd6397f4d1a3d7a431d72d456ddfe3
Cyril Stoller <cyril.stoller@gmail.com>
2013-05-17 12:29:45 +0200

    finished exercise 6 DSV

--- DSV_Vetter/Uebung6/Null_Polstellen.m
+++ DSV_Vetter/Uebung6/Null_Polstellen.m
@@ -2,13 +2,13 @@ clear all
 close all
 %% Eingabe Pol und Nullstellen
 % Nullstellen:
-B_radius = [1 1]';
-B_freq   = [0.1 -0.1]';
+B_radius = [1/0.9 1/0.9]';
+B_freq   = [0.4 -0.4]';
 B = poly(B_radius.*exp(j*2*pi*B_freq));
 
 % Polstellen
 A_radius = [0.9 0.9]';
-A_freq   = [0.3 -0.3]';
+A_freq   = [0.4 -0.4]';
 A = poly(A_radius.*exp(j*2*pi*A_freq));
 
 %% Darstellung
