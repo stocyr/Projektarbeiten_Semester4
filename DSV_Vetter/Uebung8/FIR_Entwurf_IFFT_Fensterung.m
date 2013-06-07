6a7e0fa9f9652047bf94a2d73d38eec9e25abdf1
Cyril Stoller <cyril.stoller@gmail.com>
2013-06-07 11:12:25 +0200

    number1 in uebung 8 solved and coded properly

--- DSV_Vetter/Uebung8/FIR_Entwurf_IFFT_Fensterung.m
+++ DSV_Vetter/Uebung8/FIR_Entwurf_IFFT_Fensterung.m
@@ -11,9 +11,18 @@ t = linspace(-0.5*N, 0.5*N-1, N)';
 
 filterSpektrum = (abs(f) <= f_grenz_filtering);
 
+plot(f, filterSpektrum, 'linewidth', 3)
+grid on
+title('Soll-Filterform im Spektrum');
+xlabel('Normierte Frequenz');
+
 filterZeit = real(ifftshift(ifft(fftshift(filterSpektrum))));
 
+figure
 plot(t, filterZeit)
+grid on
+title('Filter im Zeitbereich');
+xlabel('Samples');
 
 
 b = 4;
@@ -23,10 +32,25 @@ fenster = kaiser(n_fenster, b);
 filterGefenstert = zeros(N,1);
 filterGefenstert(N/2-(n_fenster/2-1):N/2+n_fenster/2) = filterZeit(N/2-39:N/2+40) .* fenster;
 
+figure
 plot(filterGefenstert)
+grid on
+title('Filter im Zeitbereich (mit Nullen ergänzt)');
+xlabel('Samples');
+
 filterkern = fftshift(fft(fftshift(filterGefenstert)));
 
+figure
 plot(f, 20*log10(filterkern))
+grid on
+title('Filter im Spektrum');
+xlabel('Genormte Frequenz');
+ylabel('dB');
+
+% Überprüfung des Spektrums mit der internen Funktion Freqz
+figure
+freqz(filterGefenstert, 1, 1024, 1)
+title('Filter im Spektrum (mit freqz');
 
 %% Filterqualität beurteilen aufgrund der Bandspezifikationen
 %ripple_durchlassband = 20*log10(filterkern(find(f == f_grenz_soll)))
